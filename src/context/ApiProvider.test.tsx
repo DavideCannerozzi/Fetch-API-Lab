33070297cbb13302c1c53a232be3bb2c482b415e
Davide Cannerozzi <d.cannerozzi@gmail.com>
2026-07-20 10:47:36 +0200

    add more tests for apiProvider

--- src/context/ApiProvider.test.tsx
+++ src/context/ApiProvider.test.tsx
@@ -1,7 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { ApiProvider } from "./ApiProvider";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { useApiContext } from "../hooks/useApiContext";
+import userEvent from "@testing-library/user-event";
 
 function TestComponent() {
   const { selectedUrl } = useApiContext();
@@ -14,6 +15,16 @@ function ChangeState() {
     <button onClick={() => setSelectedUrl("https://api.example.com")}>
       Change State Url
     </button>
+  );
+}
+
+function HandleClick() {
+  const { handleClick, selectedUrl } = useApiContext();
+  return (
+    <>
+      <button onClick={() => handleClick("Animals")}>Click</button>
+      <div data-testid="updated-url">{selectedUrl}</div>
+    </>
   );
 }
 
@@ -39,13 +50,26 @@ describe("Api Provider Context", () => {
     expect(screen.getByTestId("selected-url")).toHaveTextContent("");
   });
 
-  it("local storage should have new updated url inside the useEffect", () => {
+  it("should update localStorage when selectedUrl changes", async () => {
+    const user = userEvent.setup();
     render(
       <ApiProvider>
         <ChangeState />
       </ApiProvider>,
     );
-    fireEvent.click(screen.getByRole("button"));
+    await user.click(screen.getByRole("button"));
     expect(localStorage.getItem("selectedUrl")).toBe("https://api.example.com");
+  });
+  it("should update selectedUrl when an existing category is clicked", async () => {
+    const user = userEvent.setup();
+    render(
+      <ApiProvider>
+        <HandleClick />
+      </ApiProvider>,
+    );
+    await user.click(screen.getByRole("button", { name: "Click" }));
+    expect(screen.getByTestId("updated-url")).toHaveTextContent(
+      "https://api.thecatapi.com/v1/images/search",
+    );
   });
 });
